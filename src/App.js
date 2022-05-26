cd312fef699c9cc7c13ccbafee718100eef5fcc8
David Siddle <siddav83@gmail.com>
2022-05-26 22:40:29 +0100

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState, useReducer} from 'react';
+
+const ACTION = {
+  ADD_TODO: 'add-todo'
+}
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTION.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+  } 
+}
+  const newTodo = (name) => {
+    return {id: Date.now(), name:name, complete:false}
+  }
+
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer,[])
+  const [name, setName] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch({type: ACTION.ADD_TODO, payload: {name:name}})
+    setName("")
+  }
+
+  console.log(todos)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+    </form>
   );
 }
 
